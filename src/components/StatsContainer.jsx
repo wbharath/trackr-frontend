acf0d98f39cd12b048806bc2cacbdbc5ea974fad
wbharath <wbharath51@gmail.com>
2026-03-12 18:08:43 -0300

    stats container- added 4th category

--- src/components/StatsContainer.jsx
+++ src/components/StatsContainer.jsx
@@ -1,30 +1,43 @@
-import { FaBug, FaCalendarCheck, FaSuitcaseRolling } from 'react-icons/fa'
+import {
+  FaBug,
+  FaCalendarCheck,
+  FaSuitcaseRolling,
+  FaTrophy
+} from 'react-icons/fa'
 import Wrapper from '../assets/wrappers/StatsContainer'
 import { useSelector } from 'react-redux'
 import StatItem from './StatItem'
+
 const StatsContainer = () => {
   const { stats } = useSelector((store) => store.allJobs)
   const defaultStats = [
     {
       title: 'pending applications',
-      count: stats.pending,
+      count: stats.APPLIED || 0,
       icon: <FaSuitcaseRolling />,
       color: '#e9b949',
       bcg: '#fcefc7'
     },
     {
       title: 'interviews scheduled',
-      count: stats.interview,
+      count: stats.INTERVIEW || 0,
       icon: <FaCalendarCheck />,
       color: '#647acb',
       bcg: '#e0e8f9'
     },
     {
       title: 'jobs declined',
-      count: stats.declined,
+      count: stats.REJECTED || 0,
       icon: <FaBug />,
       color: '#d66a6a',
       bcg: '#ffeeee'
+    },
+    {
+      title: 'offers received',
+      count: stats.OFFER || 0,
+      icon: <FaTrophy />,
+      color: '#2cb67d',
+      bcg: '#c8f7e4'
     }
   ]
   return (
